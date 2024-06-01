cdaf6b04b06ff5812531998aee527ce3a310a21b
LennardKittner <lennard@kittner.dev>
2024-06-01 12:47:12 +0200

    Added more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::f64;
+use std::{cmp, f64};
 use std::mem::size_of;
 
 type Unit = u64;
@@ -57,7 +57,7 @@ impl BitVector {
         //TODO: maybe size optimization
         //TODO: maybe avoid access
         //TODO: out of bounds access
-        self.block_size = ((self.len as f64).log2() / 2f64) as usize;
+        self.block_size = cmp::max(((self.len as f64).log2() / 2f64) as usize, 1);
         self.super_block_size = self.block_size.pow(2);
 
         // generate super blocks
@@ -120,6 +120,9 @@ impl BitVector {
 #[cfg(test)]
 pub mod test {
     use crate::BitVector;
+    use rand::Rng;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
 
     #[test]
     fn test_load_from_string_and_access() {
@@ -131,9 +134,7 @@ pub mod test {
         }
     }
 
-    #[test]
-    fn test_init() {
-        let data = "01001000101010000111101010101111100100001011100011100011010101001101010010101011111000011010110101010111110101010111000011101110";
+    fn test_init(data: &str) {
         let mut bit_vector = BitVector::load_from_string(data);
         bit_vector.init();
         for (i, super_block) in bit_vector.super_blocks.iter().enumerate() {
@@ -152,6 +153,32 @@ pub mod test {
                 sum += bit_vector.access(current_bit);
             }
             assert_eq!(&sum, block, "current_super_block: {current_super_block}, current_block: {current_block}");
+        }
+    }
+
+    #[test]
+    fn test_init_power_of_two_random() {
+        let data = "01001000101010000111101010101111100100001011100011100011010101001101010010101011111000011010110101010111110101010111000011101110";
+        test_init(data);
+    }
+
+    #[test]
+    fn test_init_not_power_of_two_random() {
+        let data = "010010001010100001111010101011111001000010111000111000110101010011010100101010111110000110101101010101111101010101110000111011100110110101110101111";
+        test_init(data);
+    }
+
+    #[test]
+    fn test_init_random_large() {
+        let mut data = String::new();
+        let mut rng = ChaCha8Rng::seed_from_u64(1234567);
+        for i in 0..4096 {
+            if rng.gen_range(0..=1) == 1 {
+                data += "1";
+            } else {
+                data += "0";
+            }
+            test_init(&data);
         }
     }
 }
