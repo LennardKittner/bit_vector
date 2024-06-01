5f06c555535109f2f5b8c531eb866515fa52c35b
LennardKittner <lennard@kittner.dev>
2024-06-01 15:10:57 +0200

    Implemented rank

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,41 @@ type Unit = u64;
 const UNIT_SIZE_BITS: usize = size_of::<Unit>()*8;
 const UNIT_SIZE_BYTES: usize = size_of::<Unit>();
 
+const MASK_LOOKUP: [u32; 32] = [
+    0b00000000000000000000000000000000,
+    0b00000000000000000000000000000001,
+    0b00000000000000000000000000000011,
+    0b00000000000000000000000000000111,
+    0b00000000000000000000000000001111,
+    0b00000000000000000000000000011111,
+    0b00000000000000000000000000111111,
+    0b00000000000000000000000001111111,
+    0b00000000000000000000000011111111,
+    0b00000000000000000000000111111111,
+    0b00000000000000000000001111111111,
+    0b00000000000000000000011111111111,
+    0b00000000000000000000111111111111,
+    0b00000000000000000001111111111111,
+    0b00000000000000000011111111111111,
+    0b00000000000000000111111111111111,
+    0b00000000000000001111111111111111,
+    0b00000000000000011111111111111111,
+    0b00000000000000111111111111111111,
+    0b00000000000001111111111111111111,
+    0b00000000000011111111111111111111,
+    0b00000000000111111111111111111111,
+    0b00000000001111111111111111111111,
+    0b00000000011111111111111111111111,
+    0b00000000111111111111111111111111,
+    0b00000001111111111111111111111111,
+    0b00000011111111111111111111111111,
+    0b00000111111111111111111111111111,
+    0b00001111111111111111111111111111,
+    0b00011111111111111111111111111111,
+    0b00111111111111111111111111111111,
+    0b01111111111111111111111111111111,
+];
+
 pub struct BitVector {
     data: Vec<Unit>,
     // holds blocks worst case need 10bit per entry
@@ -102,10 +137,37 @@ impl BitVector {
         ((self.data[vec_index] >> unit_index) & 1) as usize
     }
 
+    #[inline]
+    fn get_ones(block: u32, index: usize) -> usize {
+        let block = block & MASK_LOOKUP[index];
+        block.count_ones() as usize
+    }
+
+    #[inline]
+    fn get_block(&self, index: usize) -> u32 {
+        //TODO: avoid access
+        let pos = (index / self.block_size) * self.block_size;
+
+        let mut result = 0;
+        for i in pos..(pos + self.block_size) {
+            result |= self.access(i) << (i - pos)
+        }
+        result as u32
+    }
+
+
     // get number of 0/1 before index
     #[inline]
     pub fn rank(&self, bit: bool, index: usize) -> usize {
-        todo!("rank")
+        let super_block = index / self.super_block_size;
+        let block = index / self.block_size;
+
+        let result1 = self.super_blocks.get(super_block.wrapping_sub(1)).unwrap_or(&0);
+        let result2 = if block % (self.super_block_size / self.block_size) == 0 { 0 } else { self.blocks[block - 1] as usize };
+        let result3 = Self::get_ones(self.get_block(index), index % self.block_size);
+        let result = result1 + result2 + result3;
+
+        if bit { result } else { index - result }
     }
 
     // get position of index-th 0/1
@@ -170,13 +232,26 @@ pub mod test {
     fn test_init_random_large() {
         let mut data = String::new();
         let mut rng = ChaCha8Rng::seed_from_u64(1234567);
-        for i in 0..4096 {
+        for _i in 0..4096 {
             if rng.gen_range(0..=1) == 1 {
                 data += "1";
             } else {
                 data += "0";
             }
             test_init(&data);
+        }
+    }
+
+    #[test]
+    fn test_rank() {
+        let data = "010010001010100001111010101011111001000010111000111000110101010011010100101010111110000110101101010101111101010101110000111011100110110101110101111";
+        let mut bit_vector = BitVector::load_from_string(data);
+        bit_vector.init();
+        let mut sum = 0;
+        for i in 0..data.len() {
+            assert_eq!(bit_vector.rank(true, i), sum);
+            assert_eq!(bit_vector.rank(false, i), i - sum);
+            sum += bit_vector.access(i);
         }
     }
 }
